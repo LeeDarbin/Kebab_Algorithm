7671beb97a842296568da9cb4c8d057450240c15
LeeDarbin <100140035+LeeDarbin@users.noreply.github.com>
2025-07-11 18:16:44 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int a, b, c;
+int main(){
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+
+    cin >> c;
+    cout << (1 + c)*c/2;
+}
