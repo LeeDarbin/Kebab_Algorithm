c6daf8aca55f2921920d85b8202587101cc63b9f
LeeDarbin <100140035+LeeDarbin@users.noreply.github.com>
2025-08-12 11:39:36 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <queue>
+using namespace std;
+
+int n;
+queue<int>q;
+int main(){
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        q.push(i);
+    }
+    while(q.size() > 1){
+        q.pop();
+        q.push(q.front());
+        q.pop();
+    }
+    cout << q.front();
+}
